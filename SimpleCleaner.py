4d4f92d160e7d1b86f6984bd25414f070c2e6f52
Adwhweu <122550443+Adwhweu@users.noreply.github.com>
2023-12-02 22:53:45 +0300

    main

--- SimpleCleaner.py
+++ SimpleCleaner.py
@@ -11,6 +11,7 @@ import tkthread
 import pyglet
 
 
+
 def clean():
     try:
         process_list = psutil.process_iter()
@@ -32,16 +33,19 @@ def playnotify():
     except:
         pass
 
-
+def valid(inp):
+    return inp.isdigit()
 
 
 class GUI:
     def __init__(self):
-        ctk.set_default_color_theme("blue")
+        ctk.set_default_color_theme("theme.json")
         ctk.set_appearance_mode("dark")
 
         self.window = ctk.CTk()
         self.window.title("Simple Cleaner")
+        pywinstyles.apply_style(self.window, 'mica')
+        self.window.resizable(False, False)
 
         self.xa = ctk.StringVar(value="off")
         self.xa2 = ctk.StringVar(value="off")
@@ -49,6 +53,8 @@ class GUI:
         self.xa4 = ctk.StringVar()
         self.xa5 = ctk.StringVar()
         self.xa6 = ctk.StringVar()
+        self.vad = ctk.StringVar()
+        self.validcmd = self.window.register(valid)
 
         self.tabview = ctk.CTkTabview(self.window, border_width=1)
         self.tabview.pack()
@@ -56,7 +62,6 @@ class GUI:
         self.tab_1 = self.tabview.add("Main")
         self.tab_2 = self.tabview.add("Settings")
 
-        pywinstyles.apply_style(self.tab_1, 'mica')
 
         self.window.iconbitmap("logo1.ico")
         self.window.config()
@@ -79,7 +84,7 @@ class GUI:
         self.fr = ctk.CTkFrame(self.tab_2, height=20)
         self.fr.pack()
 
-        self.inpp = ctk.CTkEntry(self.fr, width=50)
+        self.inpp = ctk.CTkEntry(self.fr, width=50, validate='key', validatecommand=(self.validcmd,'%S'))
         self.inpp.grid(row=0, column=1)
 
         self.taucl = ctk.CTkCheckBox(self.fr, variable=self.xa, width=25, text="Clean when above: ", onvalue="on", offvalue="off")
@@ -88,7 +93,7 @@ class GUI:
         self.fr2 = ctk.CTkFrame(self.tab_2, height=20)
         self.fr2.pack()
 
-        self.ntfe = ctk.CTkEntry(self.fr2, width=50)
+        self.ntfe = ctk.CTkEntry(self.fr2, width=50, validate='key', validatecommand=(self.validcmd,'%S'))
         self.ntfe.grid(row=0, column=1)
 
         self.chkd = ctk.CTkCheckBox(self.fr2, variable=self.xa2, width=25, text="Notify when above: ", onvalue="on", offvalue="off")
@@ -100,21 +105,24 @@ class GUI:
         self.fr3 = ctk.CTkFrame(self.tab_2, height=40)
         self.fr3.pack()
 
-        self.dangerpercl = ctk.CTkLabel(self.fr3, text="⚠️ Dangerous threshold  ")
+
+        self.dangerpercl = ctk.CTkLabel(self.fr3, text="⚠️ Dangerous threshold  ", text_color="orange")
         self.dangerpercl.grid(row=0, column=0)
 
-        self.dangerperc = ctk.CTkEntry(self.fr3, width=35)
+        self.dangerperc = ctk.CTkEntry(self.fr3, width=35, validate='key', validatecommand=(self.validcmd,'%S'))
         self.dangerperc.grid(row=0, column=1)
 
-
-        self.critpercl = ctk.CTkLabel(self.fr3, text="⚠️ Critical threshold  ")
+        self.critpercl = ctk.CTkLabel(self.fr3, text="⚠️ Critical threshold  ", text_color="red")
         self.critpercl.grid(row=1, column=0)
 
-        self.critperc = ctk.CTkEntry(self.fr3, width=35)
+        self.critperc = ctk.CTkEntry(self.fr3, width=35, validate='key', validatecommand=(self.validcmd,'%S'))
         self.critperc.grid(row=1, column=1)
 
 
-        self.update_memory_bar()
+        try:
+            self.update_memory_bar()
+        except:
+            pass
 
 
     def cauto(self, a, b, c):
@@ -144,10 +152,31 @@ class GUI:
         mem1 = round(number=used_memory / GB, ndigits=1)
         mem2 = round(number=memory.total / GB, ndigits=1)
 
-        if not self.dangerperc.get():
+        aa = self.dangerperc.get()
+        bb = self.critperc.get()
+
+
+        if not aa:
+            self.dangerperc.delete(0, 'end')
             self.dangerperc.insert(0, "60")
-        if not self.critperc.get():
+        else:
+            if int(aa) <= 0:
+                self.dangerperc.delete(0, 'end')
+                self.dangerperc.insert(0, "60")
+            elif int(aa) > 100:
+                self.dangerperc.delete(0, 'end')
+                self.dangerperc.insert(0, "60")
+
+        if not bb:
+            self.critperc.delete(0, 'end')
             self.critperc.insert(0, "90")
+        else:
+            if int(bb) <= 0:
+                self.critperc.delete(0, 'end')
+                self.critperc.insert(0, "90")
+            elif int(bb) < 100:
+                self.critperc.delete(0, 'end')
+                self.critperc.insert(0, "90")
 
 
         try:
@@ -168,7 +197,10 @@ class GUI:
 
         self.memory_bar.create_rectangle(bar_width, 0, 300, bar_height, fill='green')
 
-        self.window.after(1000, self.update_memory_bar)
+        try:
+            self.window.after(1000, self.update_memory_bar)
+        except:
+            pass
 
         self.cauto(a=self.xa.get(), b=str(round(memory_percent)), c=str(self.inpp.get()))
         self.notify(a=self.xa2.get(), b=str(round(memory_percent)), c=str(self.ntfe.get()))
@@ -184,14 +216,14 @@ class GUI:
 
 def open_menu():
     g = GUI()
-    a = tkthread.call(g.run)
+    tkthread.call(g.run)
 
 
 def maid():
     global icon
     image = PIL.Image.open('logo1.png')
     icon = pystray.Icon("Simple Cleaner", image, menu=pystray.Menu(
-            pystray.MenuItem("Open", open_menu),
+            pystray.MenuItem("Open", open_menu, default=True),
             pystray.MenuItem("Clean", clean),
             pystray.MenuItem("Close", close)
     ))
@@ -201,7 +233,7 @@ def close():
     global icon
     g = GUI()
     icon.stop()
-    tkthread.call_nosync(g.stop)
+    g.stop()
     sys.exit()
 
 
